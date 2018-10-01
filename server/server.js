8bdd6dfc8e0677b0adfd79a81187325cd9549447
malik <mdmalik.b@gmail.com>
2018-10-01 21:11:02 +0800

    Adding sockets

--- server/server.js
+++ server/server.js
@@ -1,20 +1,26 @@
+//Packages
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+//Variables
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
-
-//app.use(bodyParser.json());
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-/*app.get('/',(req,res) =>{
-  res.render('index.html');
+io.on('connection',(socket) => {
+  console.log('New user connected');
 
-});*/
+  socket.on('disconnect',() =>{
+    console.log('User disconnected');
+  });
+});
 
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
