c0acb8904c0262a6ff4ef1c643a03431f70f64d5
malik <mdmalik.b@gmail.com>
2018-10-02 00:09:20 +0800

    Added socket emits

--- server/server.js
+++ server/server.js
@@ -16,6 +16,26 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
   console.log('New user connected');
 
+  socket.emit('newEmail', {
+    from:'malik@example.com',
+    text:'whats up',
+    createdAt:123
+  });
+
+  socket.emit('newMessage', {
+    from:'malik@text.com',
+    text:'whats up Malik',
+    createdAt:123
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail',newEmail);
+  })
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage',newMessage);
+  })
+
   socket.on('disconnect',() =>{
     console.log('User disconnected');
   });
