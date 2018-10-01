19c62672fe382e95a8388b370f79303b42242513
malik <mdmalik.b@gmail.com>
2018-10-02 00:24:09 +0800

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,27 +3,13 @@ var socket = io();
 socket.on('connect', function()  {
   console.log('Connected to server');
 
-  socket.emit('createEmail',{
-    to:'jen@example.com',
-    text:'Hi buster'
-  });
-
-  socket.emit('createMessage',{
-    from:'jen@text.com',
-    text:'Hi ceiling'
-  });
-
 });
 
 socket.on('disconnect', function()  {
   console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function(email) {
-  console.log('New Email', email);
 
 socket.on('newMessage', function(message){
   console.log('New Message', message);
-});
-
 });
