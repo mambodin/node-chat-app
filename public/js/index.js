762961f5b1c20237b827def1f9df76bb0040e9ab
malik <mdmalik.b@gmail.com>
2018-10-03 03:04:09 +0800

    Added scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+    //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollheight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollheight);
+  }
+}
+
 socket.on('connect', function()  {
   console.log('Connected to server');
 
@@ -20,6 +36,7 @@ socket.on('newMessage', function(message){
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
 });
 
@@ -33,7 +50,7 @@ socket.on('newLocationMessage', function (message){
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
