0b15b7d36117da37299415340d2b6de3dd22032d
malik <mdmalik.b@gmail.com>
2018-10-03 00:16:03 +0800

    Test Build 031018

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -13,6 +13,22 @@ describe('generateMessage', () => {
         from,
         text
       });
+
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should return a location message object', () => {
+    var from = 'Admin';
+    var longitude = 123;
+    var latitude = 54848;
+    var url = 'https://www.google.com/maps/?q=54848,123';
+    var message = generateLocationMessage(from,latitude,longitude);
+
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,url});
+
 
   });
 });
